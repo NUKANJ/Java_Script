747da90e76d5e3535b78d6f5a21395165c733c50
NUKANJ <P7167530@ness.com>
2025-11-17 08:40:00 +0000

    new

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -2,3 +2,17 @@ const accountId = 12345
 let accountEmail = "ner456@gmail.com"
 var accountPassword = "123"
 accountCity = "Jaipur"
+accountEmail = "neruneru@gmail.com"
+accountPassword = "123"
+let accountState;
+
+
+// accountId = 2
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
