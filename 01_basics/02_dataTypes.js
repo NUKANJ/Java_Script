97dde0ddc65a1371559165e5e63f1835f5a18e95
NUKANJ <P7167530@ness.com>
2025-11-18 17:06:05 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,29 @@
 "use strict";// treat all js code as new version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Rahul")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof"Rahul");
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
