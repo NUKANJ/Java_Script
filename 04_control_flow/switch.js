34726bb844dbdcdd90aedd19baf15ad4b1d8eace
NUKANJ <P7167530@ness.com>
2026-01-10 08:32:39 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,40 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("January");
+        break;
+    case 3:
+        console.log("Feb");
+        break;
+    case 4:
+        console.log("Mar");
+        break;
+    case 5:
+        console.log("Apr");
+        break;
+    case 6:
+        console.log("May");
+        break;
+    case 7:
+        console.log("June");
+        break;
+    case 8:
+        console.log("July");
+        break;
+
+    default:
+        break;
+}
